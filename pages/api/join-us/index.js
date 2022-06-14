8086c41b996b25e93158bf48d3f05677777660e8
Jahid Hasan <jahidhasan2856@gmail.com>
2022-06-14 11:26:31 +0600

    some api update

--- pages/api/join-us/index.js
+++ pages/api/join-us/index.js
@@ -1,94 +1,98 @@
 import nc from "next-connect";
 import Joinus from "../../../models/Joinus";
 import db from "../../../utils/db";
+import multer from "multer";
+// import streamifier from 'streamifier';
+import { v2 as cloudinary } from 'cloudinary';
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-// export const config = {
-//     api: {
-//         bodyParser: false,
-//     },
-// };
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
 
 const handler = nc();
-// const upload = multer();
+const upload = multer();
 
-handler.post(async (req, res) => {
-  const {
-    full_name,
-    email,
-    city,
-    phone,
-    profile_picture,
-    about,
-    website,
-    instagram,
-    linkedin,
-    other_link,
-    project_title1,
-    skill1,
-    project_link1,
-    tools1,
-    project_image1,
-    project_title2,
-    skill2,
-    project_link2,
-    tools2,
-    project_image2,
-    project_title3,
-    skill3,
-    project_link3,
-    tools3,
-    project_image3,
-  } = req.body;
+handler.use(upload.single("project_image1")).post(async (req, res) => {
+  console.log(req.body)
+  // const {
+  //   full_name,
+  //   email,
+  //   city,
+  //   phone,
+  //   profile_picture,
+  //   about,
+  //   website,
+  //   instagram,
+  //   linkedin,
+  //   other_link,
+  //   project_title1,
+  //   skill1,
+  //   project_link1,
+  //   tools1,
+  //   project_image1,
+  //   project_title2,
+  //   skill2,
+  //   project_link2,
+  //   tools2,
+  //   project_image2,
+  //   project_title3,
+  //   skill3,
+  //   project_link3,
+  //   tools3,
+  //   project_image3,
+  // } = req.body;
 
   await db.connect();
 
-  const joinUs = new Joinus({
-    full_name,
-    email,
-    city,
-    phone,
-    profile_picture,
-    about,
-    website,
-    instagram,
-    linkedin,
-    other_link,
-    projects: [
-      {
-        project1: {
-          project_title: project_title1,
-          skill: skill1,
-          tools: tools1,
-          project_link: project_link1,
-          project_image: project_image1,
-        },
-      },
-      {
-        project2: {
-          project_title: project_title2,
-          skill: skill2,
-          tools: tools2,
-          project_link: project_link2,
-          project_image: project_image2,
-        },
-      },
-      {
-        project3: {
-          project_title: project_title3,
-          skill: skill3,
-          tools: tools3,
-          project_link: project_link3,
-          project_image: project_image3,
-        },
-      },
-    ],
-  });
+  // const joinUs = new Joinus({
+  //   full_name,
+  //   email,
+  //   city,
+  //   phone,
+  //   profile_picture,
+  //   about,
+  //   website,
+  //   instagram,
+  //   linkedin,
+  //   other_link,
+  //   projects: [
+  //     {
+  //       project1: {
+  //         project_title: project_title1,
+  //         skill: skill1,
+  //         tools: tools1,
+  //         project_link: project_link1,
+  //         project_image: project_image1,
+  //       },
+  //     },
+  //     {
+  //       project2: {
+  //         project_title: project_title2,
+  //         skill: skill2,
+  //         tools: tools2,
+  //         project_link: project_link2,
+  //         project_image: project_image2,
+  //       },
+  //     },
+  //     {
+  //       project3: {
+  //         project_title: project_title3,
+  //         skill: skill3,
+  //         tools: tools3,
+  //         project_link: project_link3,
+  //         project_image: project_image3,
+  //       },
+  //     },
+  //   ],
+  // });
 
   if (await joinUs.save()) {
     await db.disconnect();
